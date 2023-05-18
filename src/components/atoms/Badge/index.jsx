893b3502298f419ba4d34589d8884dd360bf89fa
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-05-18 15:07:15 +0700

    chore: reformat code structure

--- src/components/atoms/Badge/index.jsx
+++ src/components/atoms/Badge/index.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { getVariant } from "./constant";
 
 function Badge({ variant, text }) {
-	return (
-		<div
-			className={`badge ${getVariant(variant)} rounded border-none capitalize`}
-		>
-			{text}
-		</div>
-	);
+  return (
+    <div
+      className={`badge ${getVariant(variant)} rounded border-none capitalize`}
+    >
+      {text}
+    </div>
+  );
 }
 
 export default Badge;
--- src/components/organism/Table/columns.jsx
+++ src/components/organism/Table/columns.jsx
@@ -2,42 +2,42 @@ import { makeRupiahValue, transformDate } from "../../../utils/helperMethod";
 import Badge from "../../atoms/Badge";
 
 export const LIST_COLUMN_ORDERS = [
-	{
-		Header: "ID",
-		accessor: "id",
-		Cell: ({ row: { original } }) => (
-			<section>#{original.id.slice(0, 5)}</section>
-		),
-	},
-	{
-		Header: "Date",
-		accessor: "date",
-		Cell: ({ row: { original } }) => (
-			<section>{transformDate(original.date)}</section>
-		),
-	},
-	{
-		Header: "Course",
-		accessor: "course",
-	},
-	{
-		Header: "Payment",
-		accessor: "payment",
-		Cell: ({ row: { original } }) => (
-			<Badge variant={original.payment} text={original.payment} />
-		),
-	},
-	{
-		Header: "Total",
-		accessor: "total",
-		Cell: ({ row: { original } }) => {
-			const price = makeRupiahValue(original.total);
+  {
+    Header: "ID",
+    accessor: "id",
+    Cell: ({ row: { original } }) => (
+      <section>#{original.id.slice(0, 5)}</section>
+    ),
+  },
+  {
+    Header: "Date",
+    accessor: "date",
+    Cell: ({ row: { original } }) => (
+      <section>{transformDate(original.date)}</section>
+    ),
+  },
+  {
+    Header: "Course",
+    accessor: "course",
+  },
+  {
+    Header: "Payment",
+    accessor: "payment",
+    Cell: ({ row: { original } }) => (
+      <Badge variant={original.payment} text={original.payment} />
+    ),
+  },
+  {
+    Header: "Total",
+    accessor: "total",
+    Cell: ({ row: { original } }) => {
+      const price = makeRupiahValue(original.total);
 
-			return (
-				<section className="flex justify-center gap-3 font-bold">
-					{price}
-				</section>
-			);
-		},
-	},
+      return (
+        <section className="flex justify-center gap-3 font-bold">
+          {price}
+        </section>
+      );
+    },
+  },
 ];
