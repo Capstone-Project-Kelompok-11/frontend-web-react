61f6ed6103883e83277aec54b35974dcc4c27b88
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-05-18 09:39:31 +0700

    style: use poppins font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        "custom-yellow": "#FFDA6A",
-        "custom-blue": "#3D8BFD",
-        "logo": "#0A58CA",
-      },
-    },
-  },
-  daisyui: {
-    themes: [],
-  },
-  plugins: [require("daisyui")],
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				"custom-yellow": "#FFDA6A",
+				"custom-blue": "#3D8BFD",
+				logo: "#0A58CA",
+			},
+			fontFamily: {
+				poppins: ["Poppins", ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	daisyui: {
+		themes: [],
+	},
+	plugins: [require("daisyui")],
 };
