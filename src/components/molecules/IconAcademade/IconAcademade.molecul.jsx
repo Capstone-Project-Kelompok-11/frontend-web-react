007c2dabe21c90a3988c88be744483359bfa775f
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-05-18 14:36:52 +0700

    style: change font size

--- src/components/molecules/IconAcademade/IconAcademade.molecul.jsx
+++ src/components/molecules/IconAcademade/IconAcademade.molecul.jsx
@@ -5,7 +5,7 @@ function IconAcademade() {
   return (
 		<section className="flex items-center gap-2">
 			<img src={logo} alt="Academade" className="w-12 h-12" />
-			<h1 className="font-bold text-2xl text-primary-70">AcadeMade</h1>
+			<h1 className="font-bold text-3xl text-primary-70">AcadeMade</h1>
 		</section>
 	);
 }
