9f07cbf150bd77364a26a40ceb8254db8ccc9f24
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-05-18 13:58:44 +0700

    refactor: use tag Link instead of anchor tag

--- src/components/organism/SideBar.organism.jsx
+++ src/components/organism/SideBar.organism.jsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import DashboardIcon from "../atoms/Icons/DashboardIcon.atom";
 import CourseIcon from "../atoms/Icons/CourseIcon.atom";
 import ReportingIcon from "../atoms/Icons/ReportingIcon.atom";
@@ -27,17 +27,16 @@ function SideBar() {
 		<section className="absolute block top-0 bg-primary-30 w-72 h-screen ">
 			<section className="text-center items-center flex-row ">
 				{nav.map((item) => (
-					<a key={item.name} href={item.href}>
-						<div
-							key={item.name}
-							className={`inline-flex relative px-4 w-64 mx-8 top-40 my-2 py-2 ${
-								item.href === location.pathname ? "bg-white font-bold" : ""
-							} `}
-						>
-							<div>{item.icon}</div>
-							<h1 className="mx-4 text-xl">{item.name}</h1>
-						</div>
-					</a>
+					<Link
+						key={item.name}
+						to={item.href}
+						className={`inline-flex relative px-4 w-64 mx-8 top-40 my-2 py-2 ${
+							item.href === location.pathname ? "bg-white font-bold" : ""
+						} `}
+					>
+						<div>{item.icon}</div>
+						<h1 className="mx-4 text-xl">{item.name}</h1>
+					</Link>
 				))}
 			</section>
 		</section>
