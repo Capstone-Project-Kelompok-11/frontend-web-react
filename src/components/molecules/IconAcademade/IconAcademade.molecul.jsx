11d17181044bf9e579d9e4cc3699e10155b8727e
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-05-18 10:08:07 +0700

    style: fix styling of icon academade

--- src/components/molecules/IconAcademade/IconAcademade.molecul.jsx
+++ src/components/molecules/IconAcademade/IconAcademade.molecul.jsx
@@ -3,11 +3,11 @@ import logo from "../../../assets/iconbook.png"
 
 function IconAcademade() {
   return (
-    <div className='px-8 justify-center flex flex-row'>
-    <img src={logo} alt="Academade" width={60}  className='py-12 -mt-2' />
-    <h1 className='pt-12 font-bold text-2xl text-logo'>AcadeMade</h1>
-    </div>
-  )
+		<section className="flex items-center gap-2">
+			<img src={logo} alt="Academade" className="w-12 h-12" />
+			<h1 className="font-bold text-2xl text-primary-70">AcadeMade</h1>
+		</section>
+	);
 }
 
 export default IconAcademade
