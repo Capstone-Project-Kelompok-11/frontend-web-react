74c88b0f6ca63f7660c220250c7b728d18c95bb2
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-05-18 14:45:58 +0700

    refactor: use tag link instead of section so user can go to dashboard by clicking AcadaMade

--- src/components/molecules/IconAcademade/IconAcademade.molecul.jsx
+++ src/components/molecules/IconAcademade/IconAcademade.molecul.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import logo from "../../../assets/iconbook.png"
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../../assets/iconbook.png";
 
 function IconAcademade() {
-  return (
-		<section className="flex items-center gap-2">
+	return (
+		<Link className="flex items-center gap-2 cursor-pointer" to="/dashboard">
 			<img src={logo} alt="Academade" className="w-12 h-12" />
 			<h1 className="font-bold text-3xl text-primary-70">AcadeMade</h1>
-		</section>
+		</Link>
 	);
 }
 
-export default IconAcademade
+export default IconAcademade;
