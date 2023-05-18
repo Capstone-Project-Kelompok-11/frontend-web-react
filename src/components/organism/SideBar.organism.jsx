c8f73417cbf62e26f63c1def7ac37751ac114981
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-05-18 14:23:26 +0700

    style: fix index layer

--- src/components/organism/SideBar.organism.jsx
+++ src/components/organism/SideBar.organism.jsx
@@ -24,7 +24,7 @@ function SideBar() {
 	];
 
 	return (
-		<section className="absolute block top-0 bg-primary-30 w-72 h-screen ">
+		<section className="absolute block top-0 z-10 bg-primary-30 w-72 h-screen ">
 			<section className="text-center items-center flex-row ">
 				{nav.map((item) => (
 					<Link
