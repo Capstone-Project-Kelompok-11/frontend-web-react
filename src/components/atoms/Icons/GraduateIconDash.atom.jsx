7d4514a42ff4081de46ef9e913675b7c739ca40b
achmadhafizd <ahmadhafizd2104@gmail.com>
2023-05-20 06:39:58 +0700

    feat: Create Course, Graduate, Student Icon

---
+++ src/components/atoms/Icons/GraduateIconDash.atom.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function GraduateIconDash() {
+  return (
+    <div>
+      <svg
+        width="34"
+        height="35"
+        viewBox="0 0 42 39"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M40 23.5476V13.0238L21 2.5L2 13.0238L21 23.5476L30.88 18.6905V30.0238C30.88 33.2619 26.32 36.5 21 36.5C15.68 36.5 11.12 33.2619 11.12 30.0238V18.6905"
+          stroke="#0A0A0A"
+          strokeWidth="4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </div>
+  );
+}
+
+export default GraduateIconDash;
