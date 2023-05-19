aa806e3cd2aebb78dc9e4c4a7f3065e6924da52f
achmadhafizd <ahmadhafizd2104@gmail.com>
2023-05-20 06:49:19 +0700

    fix: change dashboard view

--- src/views/Dashboard.view.jsx
+++ src/views/Dashboard.view.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import Orders from "../components/molecules/Orders/Orders.molecule";
+import FavoriteCourse from "../components/organism/FavoriteCourse/FavoriteCourse.organism";
+import HeaderDashboard from "../components/organism/HeaderDashboard/HeaderDashboard.organism";
 
 function DashboardView() {
-  return <Orders />;
+  return (
+    <div className="mt-8">
+      <div className="flex flex-col gap-6 me-8 min-h-screen">
+        <HeaderDashboard />
+        <div className="flex gap-10 ">
+          <Orders />
+          <FavoriteCourse />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default DashboardView;
