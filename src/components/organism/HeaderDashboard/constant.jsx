343f18d0b75332934fe199ebb8a3b7616494f461
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-05-20 21:12:46 +0700

    refactor: fix import icon

--- src/components/organism/HeaderDashboard/constant.jsx
+++ src/components/organism/HeaderDashboard/constant.jsx
@@ -1,27 +1,27 @@
-import newStudentImage from "../../../assets/newstudent.svg";
-import studentImage from "../../../assets/student.svg";
-import courseImage from "../../../assets/course.svg";
-import graduateImage from "../../../assets/graduate.svg";
+import NewStudentIconDash from "../../atoms/Icons/NewStudentIconDash.atom";
+import StudentIcon from "../../atoms/Icons/StudentIconDash.atom";
+import CourseIcon from "../../atoms/Icons/CourseIconDash.atom";
+import GraduateIcon from "../../atoms/Icons/GraduateIconDash.atom";
 
 export const headerDashboardData = [
   {
     title: "New Student",
     num: 10,
-    image: newStudentImage,
+    icon: <NewStudentIconDash />,
   },
   {
     title: "Student",
     num: 50,
-    image: studentImage,
+    icon: <StudentIcon />,
   },
   {
     title: "Course",
     num: 5,
-    image: courseImage,
+    icon: <CourseIcon />,
   },
   {
     title: "Graduate",
     num: 27,
-    image: graduateImage,
+    icon: <GraduateIcon />,
   },
 ];
