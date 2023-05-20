f7b92e8bb0cf117236db566d47d3f33f50cd0058
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-05-20 20:34:23 +0700

    chore: create constant header data

---
+++ src/components/organism/HeaderDashboard/constant.js
@@ -0,0 +1,27 @@
+import newStudentImage from "../../../assets/newstudent.svg";
+import studentImage from "../../../assets/student.svg";
+import courseImage from "../../../assets/course.svg";
+import graduateImage from "../../../assets/graduate.svg";
+
+export const headerDashboardData = [
+  {
+    title: "New Student",
+    num: 10,
+    image: newStudentImage,
+  },
+  {
+    title: "Student",
+    num: 50,
+    image: studentImage,
+  },
+  {
+    title: "Course",
+    num: 5,
+    image: courseImage,
+  },
+  {
+    title: "Graduate",
+    num: 27,
+    image: graduateImage,
+  },
+];
