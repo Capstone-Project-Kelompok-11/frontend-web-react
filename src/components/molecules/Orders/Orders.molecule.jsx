0b6d72980897044e0a3cd3b6d0d52ff7f70f23e9
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-05-20 20:30:44 +0700

    chore: delete unused component

--- src/components/molecules/Orders/Orders.molecule.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import OrderTable from "../../organism/RecentOrders/OrdersTable.organism";
-import { dummyData } from "../../organism/RecentOrders/dummy";
-
-function Orders() {
-  return <OrderTable newData={dummyData} />;
-}
-
-export default Orders;
