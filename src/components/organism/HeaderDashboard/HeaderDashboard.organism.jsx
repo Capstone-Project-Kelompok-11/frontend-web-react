361e4cb170fdc4d3db621eefcb7dd99b9de85605
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-05-20 20:34:50 +0700

    refactor: refactor header dashboard

--- src/components/organism/HeaderDashboard/HeaderDashboard.organism.jsx
+++ src/components/organism/HeaderDashboard/HeaderDashboard.organism.jsx
@@ -1,16 +1,24 @@
 import React from "react";
-import NewStudentDash from "../../molecules/NewStudentDash/NewStudentDash.molecule";
-import StudentDash from "../../molecules/StudentDash/StudentDash.molecule";
-import CourseDash from "../../molecules/CourseDash/CourseDash.molecule";
-import GraduateDash from "../../molecules/GraduateDash/GraduateDash.molecule";
+import InformationHeader from "../../molecules/InformationHeader";
+import ellipse from "../../../assets/ellipse.svg";
+import { headerDashboardData } from "./constant";
 
 function HeaderDashboard() {
   return (
-    <section className="grid grid-cols-4 gap-3 py-6 drop-shadow-lg shadow-lg rounded-2xl w-12/12 bg-light-blue/20 ">
-      <NewStudentDash NewStudentStatus={10} />
-      <StudentDash StudentStatus={35} />
-      <CourseDash CourseStatus={8} />
-      <GraduateDash GraduateStatus={12} />
+    <section className="relative grid grid-cols-4 gap-3 py-5 drop-shadow-lg shadow-lg rounded-2xl w-12/12 bg-light-blue/20">
+      {headerDashboardData.map((item, idx) => (
+        <InformationHeader
+          key={item.title}
+          {...item}
+          isLast={idx + 1 === headerDashboardData.length}
+        />
+      ))}
+
+      <img
+        src={ellipse}
+        alt="ellipsis"
+        className="absolute bottom-0 w-18 h-12"
+      />
     </section>
   );
 }
