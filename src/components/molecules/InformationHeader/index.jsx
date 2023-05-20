32240edd40a2eeb0f7165f98a701e560d30991dd
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-05-20 21:13:18 +0700

    refactor: replace image with icon component

--- src/components/molecules/InformationHeader/index.jsx
+++ src/components/molecules/InformationHeader/index.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-function InformationHeader({ title, num, image, isLast }) {
+function InformationHeader({ title, num, icon, isLast }) {
   return (
     <section
       className={`flex items-center justify-center py-6 ${
         !isLast && "border-r-2 border-gray-600"
       }`}
     >
-      <img src={image} alt="new student" className="w-18 h-18" />
+      {icon}
 
       <section className="flex flex-col ps-5">
         <p>{title}</p>
