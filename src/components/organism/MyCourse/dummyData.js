0c5a091c7406db3291f10a35015bbb48718d2cf6
Rehan Maulana <rehanmaul111@gmail.com>
2023-05-21 20:06:02 +0700

    refactor: add Dummy Data list my course

---
+++ src/components/organism/MyCourse/dummyData.js
@@ -0,0 +1,20 @@
+export const listMyCourse = [
+  {
+    courseName: "UI Design",
+    coursePrice: 300000,
+    totalMaterial: 5,
+    totalCourse: 5,
+  },
+  {
+    courseName: "Data Science",
+    coursePrice: 200000,
+    totalMaterial: 6,
+    totalCourse: 4,
+  },
+  {
+    courseName: "App Developer",
+    coursePrice: 150000,
+    totalMaterial: 6,
+    totalCourse: 4,
+  },
+];
