c6442cfbc1e1efce8ac8f8f4e9ed019885affb51
Rehan Maulana <rehanmaul111@gmail.com>
2023-05-21 19:59:35 +0700

    refactor: add Product Sold Icon

--- src/components/atoms/Icons/ProductSoldIcon.atom.jsx
+++ src/components/atoms/Icons/ProductSoldIcon.atom.jsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import * as React from "react";
 
 function ProductSoldIcon(props) {
   return (
@@ -15,7 +15,7 @@ function ProductSoldIcon(props) {
         fill="#fff"
       />
     </svg>
-  )
+  );
 }
 
-export default ProductSoldIcon
+export default ProductSoldIcon;
