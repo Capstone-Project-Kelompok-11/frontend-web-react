ef4a2290a92fd0153be1c226e8bfc16302986cdc
Rehan Maulana <rehanmaul111@gmail.com>
2023-05-21 20:05:06 +0700

    refactor: add Earning Molecul

--- src/components/molecules/Earning/Earning.molecul.jsx
+++ src/components/molecules/Earning/Earning.molecul.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function Earning() {
   return (
-    <div className=' bg-blue-200 flex flex-col items-start my-2 rounded-xl px-4'>
-       <h1 className='text-base '>Earning</h1>
-       <p className='font-bold text-lg'>Rp 13.000.000</p>
+    <div className=" bg-light-blue flex flex-col items-start my-2 rounded-xl px-4">
+      <h1 className="text-base ">Earning</h1>
+      <p className="font-bold text-lg">Rp 13.000.000</p>
     </div>
-  )
+  );
 }
 
-export default Earning
+export default Earning;
