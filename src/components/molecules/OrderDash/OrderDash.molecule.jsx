4bcf2ec17b056b7a6863775b2dbf3e31e2fee07f
Rehan Maulana <rehanmaul111@gmail.com>
2023-05-21 20:03:33 +0700

    refactor: add Profile Dash

--- src/components/molecules/OrderDash/OrderDash.molecule.jsx
+++ src/components/molecules/OrderDash/OrderDash.molecule.jsx
@@ -3,10 +3,12 @@ import OrderIcon from "../../atoms/Icons/OrderIcon.atom";
 
 function OrderDash({ newOrder }) {
   return (
-    <div className="inline-flex flex-col p-2 mx-3 font-bold rounded-xl text-white w-56 bg-blue-700 ">
+    <div className="inline-flex flex-col p-2 mx-3 font-bold rounded-xl text-white w-56 bg-[url('../../src/assets/bgProfile.jpg')]">
       <div className="flex px-2 my-2 justify-between">
         <h1 className="pb-4 text-2xl">Orders</h1>
-        <div className=" rounded-full h-8 my-2 p-1 bg-white bg-opacity-30"><OrderIcon /></div>
+        <div className=" rounded-full h-9 pt-2 my-2 p-2 bg-white bg-opacity-30">
+          <OrderIcon />
+        </div>
       </div>
       <p className="text-3xl px-4 py-2">+{newOrder}</p>
     </div>
