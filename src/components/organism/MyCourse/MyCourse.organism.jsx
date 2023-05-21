cede85d1378c3488a2b0cb8a6adcb5a71900859d
Rehan Maulana <rehanmaul111@gmail.com>
2023-05-21 20:06:16 +0700

    refactor: add My Course

--- src/components/organism/MyCourse/MyCourse.organism.jsx
+++ src/components/organism/MyCourse/MyCourse.organism.jsx
@@ -1,34 +1,18 @@
 import React from "react";
 import ListMyCourse from "../../molecules/ListMyCourse/ListMyCourse.molecul";
+import { listMyCourse } from "./dummyData";
 
 function MyCourse() {
-  const listMyCourse = [
-    {
-      courseName: "UI Design",
-      coursePrice: 30000,
-      totalMaterial: 5,
-      totalCourse: 5,
-    },
-    {
-        courseName: "UI Design",
-        coursePrice: 30000,
-        totalMaterial: 5,
-        totalCourse: 5,
-      }, {
-        courseName: "UI Design",
-        coursePrice: 30000,
-        totalMaterial: 5,
-        totalCourse: 5,
-      },
-  ];
   return (
-    <div className="bg-blue-50 p-6 rounded-xl drop-shadow-lg shadow-lg">
+    <div className="bg-light-blue/20 p-6 rounded-xl drop-shadow-lg shadow-lg">
       <h1 className="font-bold text-base">My Course</h1>
       {listMyCourse.map((course) => (
-       <ListMyCourse course={course} />
+        <ListMyCourse course={course} />
       ))}
       <div>
-        <a href="/profile" className="flex justify-end text-sm text-blue-500">Lainnya</a>
+        <a href="/profile" className="flex justify-end text-sm text-primary-50">
+          Lainnya
+        </a>
       </div>
     </div>
   );
