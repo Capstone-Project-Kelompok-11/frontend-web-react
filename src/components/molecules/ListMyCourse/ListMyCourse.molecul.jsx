723adedc75532366e053fb06122f394014944bef
Rehan Maulana <rehanmaul111@gmail.com>
2023-05-21 20:05:20 +0700

    refactor: add List My Course

--- src/components/molecules/ListMyCourse/ListMyCourse.molecul.jsx
+++ src/components/molecules/ListMyCourse/ListMyCourse.molecul.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
+import React from "react";
+import { makeRupiahValue } from "../../../utils/helperMethod";
 
-function ListMyCourse({course}) {
+function ListMyCourse({ course }) {
+  const price = makeRupiahValue(course.coursePrice);
   return (
-   <div className="border border-black rounded-xl text-xs px-7 py-1 my-2">
-    <div className=" flex justify-between   ">
-      <p>{course.courseName}</p>
-      <p className="font-bold text-base ">Rp {course.coursePrice}</p>
+    <div className="border border-black rounded-xl text-xs px-7 py-1 my-2">
+      <div className=" flex justify-between   ">
+        <p>{course.courseName}</p>
+        <p className="font-bold text-base ">{price}</p>
+      </div>
+      <div className="flex gap-3 opacity-60">
+        <p>{course.totalMaterial} Material</p>
+        <p>{course.totalCourse} Course</p>
+      </div>
     </div>
-    <div className="flex gap-3 opacity-60">
-      <p>{course.totalMaterial} Material</p>
-      <p>{course.totalCourse} Course</p>
-    </div>
-  </div>
-  )
+  );
 }
 
-export default ListMyCourse
+export default ListMyCourse;
