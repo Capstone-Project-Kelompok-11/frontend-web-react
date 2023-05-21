aba2fcc10402351d0d310ec0177c7d0b9946dc6b
Rehan Maulana <rehanmaul111@gmail.com>
2023-05-21 20:06:57 +0700

    refactor: add Dummy data List Rating

--- src/components/organism/RatingCourse/dummyData.js
+++ src/components/organism/RatingCourse/dummyData.js
@@ -6,21 +6,33 @@ export const listRating = [
     name: " Doni Salmanan",
     comment:
       "Very satisfied with the learning experience at this LMS! The material presented is very clear and easy to understand.",
+    rating: 4.5,
   },
   {
     id: 2,
     imageURL:
       "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    name: " Doni Salmanan",
+    name: " Tami Aulia",
     comment:
       "Very satisfied with the learning experience at this LMS! The material presented is very clear and easy to understand.",
+    rating: 4.1,
   },
   {
     id: 3,
     imageURL:
       "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    name: " Doni Salmanan",
+    name: " Tami Aulia",
     comment:
       "Very satisfied with the learning experience at this LMS! The material presented is very clear and easy to understand.",
+    rating: 4.2,
+  },
+  {
+    id: 4,
+    imageURL:
+      "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    name: " Tami Aulia",
+    comment:
+      "Very satisfied with the learning experience at this LMS! The material presented is very clear and easy to understand.",
+    rating: 4.6,
   },
 ];
