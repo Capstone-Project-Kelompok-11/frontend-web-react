48161485d67921558e0d205ea72d0238e52b3256
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-05-23 15:28:52 +0700

    chore: import image first then render it

--- src/components/organism/CourseThumbnailCard/index.jsx
+++ src/components/organism/CourseThumbnailCard/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import DetailCourseInfo from "../../molecules/DetailCourseInfo";
+import courseThumbnail from "../../../assets/courseThumbnail.svg";
 
 function CourseThumbnailCard({ data }) {
   return (
     <section className="absolute flex flex-col gap-3 bg-white h-[500px] rounded-t-2xl">
-      <img src="/src/assets/courseThumbnail.svg" alt="course thumbnail" />
+      <img src={courseThumbnail} alt="course thumbnail" />
 
       <section className="flex flex-col gap-5 p-3 pb-10 rounded-2xl shadow-lg">
         {data.map((item) => (
