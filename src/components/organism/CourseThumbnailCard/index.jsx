11c9d5ccaa76ed8b4a86a6a39845f7bfa5a77865
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-05-23 14:27:56 +0700

    feat: create course thumbnail card component

---
+++ src/components/organism/CourseThumbnailCard/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import DetailCourseInfo from "../../molecules/DetailCourseInfo";
+
+function CourseThumbnailCard({ data }) {
+  return (
+    <section className="absolute flex flex-col gap-3 bg-white h-[500px] rounded-t-2xl">
+      <img src="/src/assets/courseThumbnail.svg" alt="course thumbnail" />
+
+      <section className="flex flex-col gap-5 p-3 pb-10 rounded-2xl shadow-lg">
+        {data.map((item) => (
+          <DetailCourseInfo key={item.title} {...item} />
+        ))}
+      </section>
+    </section>
+  );
+}
+
+export default CourseThumbnailCard;
