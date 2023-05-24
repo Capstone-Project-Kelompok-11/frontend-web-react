c5b9f5d50c5cfc4a15b329cb1f2c82fa291533cf
Vernando <mayordeblo12@gmail.com>
2023-05-24 23:56:31 +0700

    fix: fix landing page and loginview

--- src/views/LandingPage.view.jsx
+++ src/views/LandingPage.view.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import GuruSvg from "../assets/guru.svg";
+import GuruSvg from "../assets/Guru.svg";
 import LogoSvg from "../assets/logo.svg";
 import Navbar from "../components/templates/Navbar.template";
 
