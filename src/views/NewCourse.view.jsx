c845ce1c34b867c078facb3898cd9ebb1b171bfc
Adi1162 <nursholihinadip@gmail.com>
2023-05-29 22:00:20 +0700

    chore : component new course view

---
+++ src/views/NewCourse.view.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NewCourse from "../components/organism/NewCourse/NewCourse.organism";
+
+function NewCourseView() {
+  return (
+    <div>
+      <NewCourse />
+    </div>
+  );
+}
+
+export default NewCourseView;
