f8face7af4e78688698cefd996455cdcde551fc1
achmadhafizd <ahmadhafizd2104@gmail.com>
2023-05-29 21:42:21 +0700

    refactor: add Quiz header  & add submit button

--- src/components/organism/QuizForm/QuizForm.organism.jsx
+++ src/components/organism/QuizForm/QuizForm.organism.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import QuizInput from "../QuizInput/QuizInput.organism";
+import QuizHeader from "../../molecules/QuizHeader/QuizHeader.molecule";
 
-function QuizForm() {
+function QuizForm({ values, arrayHelpers, setFieldValue }) {
+  if (values.question.length < 1) {
+    return undefined;
+  }
+
   return (
-    <section className="shadow-sm shadow-gray-500 p-3 min-h-fit w-5/6">
-      <div className="flex gap-5 justify-between">
-        <QuizInput />
+    <div>
+      <QuizHeader />
+      <section className="shadow-sm shadow-gray-500 p-3 min-h-fit w-5/6 my-10">
+        <div className="flex gap-5 justify-between ">
+          <QuizInput
+            arrayHelpers={arrayHelpers}
+            values={values}
+            setFieldValue={setFieldValue}
+          />
+        </div>
+      </section>
+      <div className="flex justify-end me-16 mt-24">
+        <button type="submit" className=" bg-warning-10 px-5 py-2 rounded-lg font-bold outline-none">
+          Submit
+        </button>
       </div>
-    </section>
+    </div>
   );
 }
 
