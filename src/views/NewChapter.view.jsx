f9b89504c6678d0e39567e9263492a59fa164e8b
Adi1162 <nursholihinadip@gmail.com>
2023-05-29 21:58:52 +0700

    chore : component new chapter view

---
+++ src/views/NewChapter.view.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CreateNewChapter from "../components/molecules/CreateNewChapter/CreateNewChapter.molecules";
+
+function NewChapter() {
+  return (
+    <div>
+      <CreateNewChapter />
+    </div>
+  );
+}
+
+export default NewChapter;
