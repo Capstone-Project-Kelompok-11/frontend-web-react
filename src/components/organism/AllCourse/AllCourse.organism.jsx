345135e1150eee017238187eeae390ad5f2d4ce9
Adi1162 <nursholihinadip@gmail.com>
2023-06-01 01:01:07 +0700

    fix : component all course

--- src/components/organism/AllCourse/AllCourse.organism.jsx
+++ src/components/organism/AllCourse/AllCourse.organism.jsx
@@ -7,14 +7,14 @@ function AllCourse() {
     <div className="text-center">
       <div className="flex items-center gap-4">
         <p className="text-2xl">All Course</p>
-        <Link to="/course/newCourse">
+        <Link to="/course/newcourse">
           <button class="bg-blue-500 hover:bg-blue-700 inline-flex text-white  py-2 px-6 rounded-lg text-sm">
             New Course
           </button>
         </Link>
       </div>
-      <p className="text-center mt-12">
-        Create a New Course if You Don't Have a Course Yet
+      <p className="text-center mt-12 text-gray-500">
+        create a new course if you don't have a course yet
       </p>
     </div>
   );
