f79da648b847af29d8e69a1a204c57a939abad8f
Adi1162 <nursholihinadip@gmail.com>
2023-06-01 01:08:38 +0700

    fix : component new course view

--- src/views/NewCourse.view.jsx
+++ src/views/NewCourse.view.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import NewCourse from "../components/organism/NewCourse/NewCourse.organism";
+import NewCourse from "../components/organism/NewCourse/NewCourse.organism.jsx";
 
 function NewCourseView() {
   return (
