fb8e846bc52cc36520954dbc87147567fd80a16c
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-05-31 14:24:44 +0700

    chore: move to reporting directory

---
+++ src/views/Reporting/Reporting.view.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AllCourse from "../../components/molecules/AllCourse/AllCourse.molecule";
+
+function ReportingView() {
+  return <AllCourse />;
+}
+
+export default ReportingView;
