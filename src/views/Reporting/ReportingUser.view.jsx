7de22de8a3063b94cde221448ab3461e53e59e8e
Rehan Maulana <rehanmaul111@gmail.com>
2023-05-31 17:08:17 +0700

    feat: Add Reporting User View

---
+++ src/views/Reporting/ReportingUser.view.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { userListName } from "../../components/organism/UserListCourseCard/dummyData";
+import UserListCourseCard from "../../components/organism/UserListCourseCard/UserListCourseCard.organism";
+
+function ReportingUserView() {
+  let { course_name } = useParams();
+  return (
+    <div className="">
+      <h1 className="py-4 text-xl"> {course_name} </h1>
+      <div className="grid grid-cols-2 p-1 mr-9 px-4 rounded-lg bg-light-blue/10 border ">
+        <p>Name</p>
+        <p>Aktivitas</p>
+      </div>
+      {userListName.map((user) => (
+        <Link to={`/reporting/${course_name}/${user.name}`}>
+          <UserListCourseCard key={user.id} {...user} />
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+export default ReportingUserView;
