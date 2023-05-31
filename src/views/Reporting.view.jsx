8887ed8186b4f248a5cfb8c24cfd955c6a7eb790
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-05-31 14:24:07 +0700

    chore: delete old file

--- src/views/Reporting.view.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import AllCourse from "../components/molecules/AllCourse/AllCourse.molecule";
-
-function ReportingView() {
-  return <AllCourse />;
-}
-
-export default ReportingView;
