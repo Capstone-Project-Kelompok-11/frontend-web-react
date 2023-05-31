9586c1689aa28de5b55d7d0f29ab12707913d89a
Rehan Maulana <rehanmaul111@gmail.com>
2023-05-31 16:32:26 +0700

    feat: add Reporting View

--- src/views/Reporting.view.jsx
+++ src/views/Reporting.view.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import AllCourse from "../components/molecules/AllCourse/AllCourse.molecule";
+import { listAllCourse } from "../components/organism/ListCourse/dummyData";
+import AllCourseCard from "../components/organism/AllCourseCard/AllCourseCard.organism";
+import UserListCard from "../components/organism/UserListCard/UserListCard.organism";
+import { Link } from "react-router-dom";
 
 function ReportingView() {
-  return <AllCourse />;
+  return (
+    <div className="">
+      <h1 className="text-xl">My Course</h1>
+      <div className="mr-4 my-4  h-[32rem] overflow-y-auto">
+        {listAllCourse.map((list) => (
+          <Link to={`/reporting/${list.courseName}`}>
+            <UserListCard key={list.id} {...list} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default ReportingView;
