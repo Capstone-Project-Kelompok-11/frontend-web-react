8dd11fc40c599d62fc78dbb3db37d617ceefdbc2
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-06-01 21:08:15 +0700

    style: reformat code structure

--- src/views/Reporting/ReportingSummary.view.jsx
+++ src/views/Reporting/ReportingSummary.view.jsx
@@ -28,51 +28,54 @@ function ReportingSummary() {
     <section className="flex flex-col gap-5 me-8 min-h-screen">
       <p className="text-3xl">{detail_user}</p>
 
-      <section className="relative p-10 rounded-2xl bg-light-blue-10">
+      <section className="relative p-10 rounded-2xl bg-light-blue-10 h-48">
         <UserProfileThumbnailCard data={thumbnailCourseContent} />
 
-        <section className="flex flex-col gap-3 float-right w-[65%]">
-          <h2 className="font-bold text-3xl">Summary All Material</h2>
-          <section className="flex justify-between items-center">
-            <Link
-              to="/myfile.pdf"
-              target="_blank"
-              download
-              className=" bg-danger-10 hover:bg-danger-30 duration-500 p-4 py-2 rounded-lg font-semibold"
-            >
-              Download
-            </Link>
+        <section className="flex flex-col gap-20 float-right w-[68%]">
+          <section className="flex flex-col gap-3">
+            <h2 className="font-bold text-3xl">Summary All Material</h2>
 
-            <form onSubmit={handleSubmit} className="flex gap-3 items-center">
-              <label htmlFor="nilai">Input Nilai</label>
-              <input
-                type="number"
-                name="nilai"
-                id="nilai"
-                placeholder="00"
-                min="0"
-                max="100"
-                className="p-2 outline-none rounded-md border-2 border-secondary-10 w-12 text-center placeholder:text-center"
-              />
-            </form>
+            <section className="flex justify-between items-center">
+              <Link
+                to="/myfile.pdf"
+                target="_blank"
+                download
+                className=" bg-danger-10 hover:bg-danger-30 duration-500 p-4 py-2 rounded-lg font-semibold"
+              >
+                Download
+              </Link>
+
+              <form onSubmit={handleSubmit} className="flex gap-3 items-center">
+                <label htmlFor="nilai">Input Nilai</label>
+                <input
+                  type="number"
+                  name="nilai"
+                  id="nilai"
+                  placeholder="00"
+                  min="0"
+                  max="100"
+                  className="p-2 outline-none rounded-md border-2 border-secondary-10 w-12 text-center placeholder:text-center"
+                />
+              </form>
+            </section>
           </section>
-        </section>
-      </section>
 
-      <section className="relative flex flex-col gap-2 justify-end ml-[23rem]">
-        <h3 className="font-medium text-xl">Nilai Chapter</h3>
+          <section className="flex flex-col gap-2">
+            <h3 className="font-medium text-xl">Nilai Chapter</h3>
 
-        <section className="h-96 overflow-y-auto">
-          <section className="flex flex-col gap-3 px-2 py-3">
-            {dummyData.length ? (
-              dummyData.map((item) => (
-                <ChapterCard {...item} key={item} isReporting />
-              ))
-            ) : (
-              <p className="text-light-90 text-center">
-                There is no material chapter
-              </p>
-            )}
+            <section className="h-96 overflow-y-auto">
+              <section className="flex flex-col gap-3 px-2 py-3">
+                {dummyData.length ? (
+                  dummyData.map((item) => (
+                    <ChapterCard {...item} key={item.id} isReporting />
+                  ))
+                ) : (
+                  <p className="text-light-90 text-center">
+                    There is no material chapter
+                  </p>
+                )}
+              </section>
+            </section>
           </section>
         </section>
       </section>
