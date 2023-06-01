e863e060416d7fc2f91488ae8eb06fb61b2dc3ff
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-06-01 20:12:02 +0700

    style: add course thumbnail card's width

--- src/components/organism/CourseThumbnailCard/index.jsx
+++ src/components/organism/CourseThumbnailCard/index.jsx
@@ -4,7 +4,7 @@ import courseThumbnail from "../../../assets/courseThumbnail.svg";
 
 function CourseThumbnailCard({ data }) {
   return (
-    <section className="absolute flex flex-col gap-3 bg-white h-[500px] rounded-t-2xl">
+    <section className="absolute flex flex-col gap-3 w-full bg-white h-[500px] rounded-t-2xl">
       <img src={courseThumbnail} alt="course thumbnail" />
 
       <section className="flex flex-col gap-5 p-3 pb-10 rounded-2xl shadow-lg">
