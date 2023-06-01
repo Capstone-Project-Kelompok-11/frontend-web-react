e10cba87d7cc2f5a634bb4442c896e55dd29195e
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-06-01 10:19:25 +0700

    refactor: refactor new course and chapter view

---
+++ src/views/Chapter/NewChapter.view.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import NewChapterForm from "../../components/organism/NewChapterForm/NewChapter.organism";
+
+function NewChapter() {
+  return <NewChapterForm />;
+}
+
+export default NewChapter;
