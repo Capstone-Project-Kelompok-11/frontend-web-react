f1885963c3dc8c8637fe3f437036c1129eda9ae9
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-06-01 09:58:28 +0700

    refactor: remove button tag

--- src/components/organism/AllCourse/AllCourse.organism.jsx
+++ src/components/organism/AllCourse/AllCourse.organism.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { Link } from "react-router-dom";
 
 function AllCourse() {
@@ -7,10 +6,11 @@ function AllCourse() {
     <div className="text-center">
       <div className="flex items-center gap-4">
         <p className="text-2xl">All Course</p>
-        <Link to="/course/newcourse">
-          <button class="bg-blue-500 hover:bg-blue-700 inline-flex text-white  py-2 px-6 rounded-lg text-sm">
-            New Course
-          </button>
+        <Link
+          to="/course/new-course"
+          className="bg-primary-50 hover:bg-primary-70 duration-500 inline-flex text-white  py-2 px-6 rounded-lg text-sm"
+        >
+          New Course
         </Link>
       </div>
       <p className="text-center mt-12 text-gray-500">
