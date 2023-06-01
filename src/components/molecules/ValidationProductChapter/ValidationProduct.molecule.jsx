08b751481114d9cb8235ed027560b885d0cb5bb1
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-06-01 11:09:23 +0700

    chore: delete unused file

--- src/components/molecules/ValidationProductChapter/ValidationProduct.molecule.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import * as Yup from "yup";
-
-const ValidationProduct = Yup.object().shape({
-  courseName: Yup.string().required(),
-  createDes: Yup.string().required(),
-});
-
-export default ValidationProduct;
