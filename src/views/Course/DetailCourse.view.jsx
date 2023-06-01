ffc3631975bc52a63eee43827edac508a0a69beb
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-06-01 20:57:04 +0700

    style: reformat structure code

--- src/views/Course/DetailCourse.view.jsx
+++ src/views/Course/DetailCourse.view.jsx
@@ -20,38 +20,40 @@ function DetailCourseView() {
     <section className="flex flex-col gap-5 me-8 min-h-screen">
       <p className="text-xl">My Course {id}</p>
 
-      <section className="relative p-10 rounded-2xl bg-warning-10">
+      <section className="relative p-10 rounded-2xl bg-warning-10 h-48">
         <CourseThumbnailCard data={thumbnailCourseContent} />
 
-        <section className="flex flex-col gap-3 float-right w-[70%]">
-          <h2 className="font-bold text-3xl">UI Design</h2>
-          <p className="text-sm">
-            User interface design (UI design) is an important part of developing
-            effective digital products. UI design involves designing an
-            easy-to-use and attractive user interface, and paying attention to
-            aspects such as navigation, layout, interaction and visual
-            aesthetics.
-          </p>
-        </section>
-      </section>
+        <section className="flex flex-col gap-20 float-right w-[70%]">
+          <section>
+            <h2 className="font-bold text-3xl">UI Design</h2>
+            <p className="text-sm">
+              User interface design (UI design) is an important part of
+              developing effective digital products. UI design involves
+              designing an easy-to-use and attractive user interface, and paying
+              attention to aspects such as navigation, layout, interaction and
+              visual aesthetics.
+            </p>
+          </section>
 
-      <section className="relative flex flex-col gap-3 justify-end ps-80">
-        <section className="flex justify-between">
-          <h3 className="font-bold text-2xl">Chapter</h3>
-          <PlusIcon width={30} height={30} onClick={handleAddChapter} />
-        </section>
+          <section className="flex flex-col gap-3">
+            <section className="flex justify-between">
+              <h3 className="font-bold text-2xl">Chapter</h3>
+              <PlusIcon width={30} height={30} onClick={handleAddChapter} />
+            </section>
 
-        <section className="h-96 overflow-y-auto">
-          <section className="flex flex-col gap-3 px-2 py-3">
-            {dummyData.length ? (
-              dummyData.map((item) => (
-                <ChapterCard {...item} key={item.id} courseId={id} />
-              ))
-            ) : (
-              <p className="text-light-90 text-center">
-                Create a new chapter if there is no material chapter
-              </p>
-            )}
+            <section className="h-96 overflow-y-auto">
+              <section className="flex flex-col gap-3 px-2 py-3">
+                {dummyData.length ? (
+                  dummyData.map((item) => (
+                    <ChapterCard {...item} key={item.id} courseId={id} />
+                  ))
+                ) : (
+                  <p className="text-light-90 text-center">
+                    Create a new chapter if there is no material chapter
+                  </p>
+                )}
+              </section>
+            </section>
           </section>
         </section>
       </section>
