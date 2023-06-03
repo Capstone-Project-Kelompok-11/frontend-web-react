c5c89b681affa75386272927437faca9c93778ee
Adi1162 <nursholihinadip@gmail.com>
2023-06-03 10:24:53 +0700

    feat: add createnewchapterview

---
+++ src/views/Chapter/CreateNewChapter.view.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CreateNewChapter from "../../components/organism/CreateNewChapter/CreateNewChapter.organism";
+
+function CreateNewChapterView() {
+  return (
+    <div>
+      <CreateNewChapter />
+    </div>
+  );
+}
+
+export default CreateNewChapterView;
