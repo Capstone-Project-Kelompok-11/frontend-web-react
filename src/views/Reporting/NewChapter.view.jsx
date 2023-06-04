7ea49bf146109c5fff16ef4d1ecaed21b57a0d07
Adi1162 <nursholihinadip@gmail.com>
2023-06-04 10:19:58 +0700

    fix: newchapter

---
+++ src/views/Reporting/NewChapter.view.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CreateNewChapter from "../../components/organism/CreateNewChapter/CreateNewChapter.organism";
+
+function NewChapterView() {
+  return (
+    <div>
+      <CreateNewChapter />
+    </div>
+  );
+}
+
+export default NewChapterView;
