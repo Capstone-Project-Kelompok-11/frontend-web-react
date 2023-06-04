3768df2aaae7a69c14e4f397f893644c43a74a7e
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-06-04 11:34:42 +0700

    chore: rename to chapter var and use font semibold for label input

--- src/components/organism/NewChapterForm/NewChapter.organism.jsx
+++ src/components/organism/NewChapterForm/NewChapter.organism.jsx
@@ -4,12 +4,12 @@ import {
   initCreateNewChapterValue,
   validationCreateNewChapter,
 } from "./constant";
+import { Link } from "react-router-dom";
 import UploadFileIcon from "../../atoms/Icons/UploadFileIcon.atom";
 import CreateQuizIcon from "../../atoms/Icons/CreateQuizIcon.atom";
 import CameraIcon from "../../atoms/Icons/CameraIcon.atom";
 import ArrowPathIcon from "../../atoms/Icons/ArrowPathIcon.atom";
-import { Link } from "react-router-dom";
-import ArrowLink from "../../atoms/Icons/ArrowLink.atom";
+import ArrowLinkIcon from "../../atoms/Icons/ArrowLinkIcon.atom";
 
 const NewChapterForm = ({ createNewChapter, data = {} }) => {
   const initData = useMemo(() =>
@@ -39,7 +39,7 @@ const NewChapterForm = ({ createNewChapter, data = {} }) => {
       <div className="w-full flex flex-row gap-6">
         <div className="w-96">
           <div className="mb-3 flex items-center">
-            <p className="font-bold">Course Thumbnail</p>
+            <p className="font-semibold">Chapter Thumbnail</p>
           </div>
           <div className="w-full h-270 bg-gray-200 flex items-center  rounded-xl justify-center rounded-10">
             <div className="image-container">
@@ -59,38 +59,38 @@ const NewChapterForm = ({ createNewChapter, data = {} }) => {
         </div>
         <div className="w-5/6 mr-10">
           <div className="mb-2">
-            <label htmlFor="courseName">
+            <label htmlFor="chapterName">
               <div className="flex items-center">
-                <p className="font-semibold">Course Name</p>
-                <span className="text-red-500">*</span>
+                <p className="font-semibold">Chapter Name</p>
+                <span className="text-red-500 font-semibold">*</span>
               </div>
             </label>
             <input
+              id="chapterName"
+              name="chapterName"
               type="text"
-              id="courseName"
-              value={formik.values.courseName}
-              onChange={formik.handleChange}
-              name="courseName"
               autoFocus
               className="w-full h-12 mt-3 bg-gray-200 p-5 rounded-lg border border-gray-500"
               placeholder="e.g. “Capstone Project 11”"
+              value={formik.values.chapterName}
+              onChange={formik.handleChange}
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="courseDes">
+            <label htmlFor="chapterDes">
               <div className="flex items-center">
-                <label className="font-bold">Create Description</label>
-                <span className="text-red-500">*</span>
+                <label className="font-semibold">Chapter Description</label>
+                <span className="text-red-500 font-semibold">*</span>
               </div>
             </label>
             <textarea
-              name="courseDes"
-              id="courseDes"
-              value={formik.values.courseDes}
-              onChange={formik.handleChange}
+              id="chapterDes"
+              name="chapterDes"
               className="resize-y w-full h-40 mt-3 bg-gray-200 p-5 rounded-lg border border-gray-500"
               placeholder="Type here..."
               rows="5"
+              value={formik.values.chapterDes}
+              onChange={formik.handleChange}
             ></textarea>
           </div>
         </div>
@@ -98,7 +98,7 @@ const NewChapterForm = ({ createNewChapter, data = {} }) => {
       <div className="w-full flex flex-row gap-6">
         <div className="w-80">
           <div className="mb-2 flex items-center">
-            <p className="font-bold">Task Quiz</p>
+            <p className="font-semibold">Task Quiz</p>
           </div>
           <div className="w-full h-40 bg-gray-200 flex items-center justify-center rounded-lg">
             <div className="flex items-center justify-center">
@@ -137,7 +137,7 @@ const NewChapterForm = ({ createNewChapter, data = {} }) => {
         </Link>
         <div className="w-80 mr-8">
           <div className="mb-2  flex items-center">
-            <p className="font-bold">Uploaded Videos</p>
+            <p className="font-semibold">Uploaded Videos</p>
           </div>
           <div className="w-full h-40 bg-gray-200 flex items-center justify-center rounded-lg">
             <div className="image-container">
@@ -147,7 +147,7 @@ const NewChapterForm = ({ createNewChapter, data = {} }) => {
                   className="flex flex-col items-center justify-center w-full h-64 "
                 >
                   <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                    <ArrowLink />
+                    <ArrowLinkIcon />
                   </div>
                   <input id="dropzone-file" type="file" className="hidden" />
                 </label>
@@ -157,7 +157,7 @@ const NewChapterForm = ({ createNewChapter, data = {} }) => {
         </div>
       </div>
 
-      <div className="flex font-bold mt-6">
+      <div className="flex font-semibold mt-6">
         <button className="justify-start  bg-warning-10 hover:bg-warning-30 text-black py-2 px-6 rounded-lg text-sm fw-bold">
           Back
         </button>
