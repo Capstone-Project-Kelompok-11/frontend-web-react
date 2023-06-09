79b2c0ef4c4341bfe4b7f361326fb4ef7b938890
nando <107538780+Vernandodo@users.noreply.github.com>
2023-06-09 17:43:52 +0700

    Feature/login (#26)
    * chore: detail courseview
    
    * feat: add route detailchapter
    
    * chore: clsx package
    
    * refactor: change route detailcourse view
    
    * refactor: change Profile_MyCourse `s route`
    
    * feat: add DetailChapter
    
    * refactor: change detail chapter `s` route
    
    * refactor: add cond rendering detail course
    
    * Merge branch 'staging' of https://github.com/Capstone-Project-Kelompok-11/frontend-web-react into feature/detail_chapter
    
    * refactor: move directory to chapter
    
    * refactor: change h1
    
    * style: formating by yarn format
    
    * chore: change route for detail course
    
    * style: add margin bottom
    
    * style: remove duplicate section and add margin bottom
    
    * style: use pointer when there is on click or not
    
    * style: reformat code structure
    
    * feat: add session reducers
    
    * refactor: change to protected route
    
    * refactor: change to protected route
    
    * chore:  install js-cookie package
    
    * feat: add utils method
    
    * refactor: add integration api
    
    * feat: add env
    
    * refactor: change schema Logout
    
    * refactor: change directory sessions
    
    * refactor: change dummy token to cookies token
    
    * refactor: change method name
    
    * refactor: change method name
    
    * chore: ignore env files
    
    * style: add bg-cover to login wrapper
    
    * refactor: change delete last slash
    
    * refactor: change styling button
    
    * refactor: change method name and space
    
    * fix: delete env
    
    * refactor: change login method
    
    * refactor: change logic route login
    
    ---------
    
    Co-authored-by: Erick Ezrandy <erick.ezrandy@ui.ac.id>

---
+++ src/utils/fetcherMethod.js
@@ -0,0 +1,76 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+const token = Cookies.get("token");
+
+const config = {
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
+  },
+};
+
+export const getRequest = async (url) => {
+  try {
+    const res = await axios.get(
+      `${import.meta.env.VITE_API_URL}${url}`,
+      config
+    );
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const login = async (value) => {
+  try {
+    await axios
+      .post(`${import.meta.env.VITE_API_URL}/api/v1/users/login`, {
+        username: "admin",
+        ...value,
+      })
+      .then((res) => {
+        Cookies.set("token", res.data.data.token);
+      });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const postRequest = async (url, value) => {
+  try {
+    const res = await axios.post(
+      `${import.meta.env.VITE_API_URL}${url}`,
+      value,
+      config
+    );
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const updateRequest = (url, value) => {
+  try {
+    const res = axios.put(
+      `${import.meta.env.VITE_API_URL}${url}`,
+      value,
+      config
+    );
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const deleteRequest = async (url) => {
+  try {
+    const res = await axios.delete(
+      `${import.meta.env.VITE_API_URL}${url}`,
+      config
+    );
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
