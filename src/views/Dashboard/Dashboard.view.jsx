4608e84fbc78d9e187dc133bd9f5e09bd8942040
achmadhafizd <ahmadhafizd2104@gmail.com>
2023-06-11 19:59:24 +0700

    refactor: destruct loading statement

--- src/views/Dashboard/Dashboard.view.jsx
+++ src/views/Dashboard/Dashboard.view.jsx
@@ -1,17 +1,52 @@
 import React from "react";
+import useSWR from "swr";
+import { getRequest } from "../../utils/fetcherMethod";
+import { transformDate } from "../../utils/helperMethod";
 import FavoriteCourse from "../../components/organism/FavoriteCourse/FavoriteCourse.organism";
 import HeaderDashboard from "../../components/organism/HeaderDashboard/HeaderDashboard.organism";
 import OrdersTable from "../../components/organism/RecentOrders/OrdersTable.organism";
-import { dummyData } from "../../components/organism/RecentOrders/dummy";
 
 function DashboardView() {
+  const { data: dataStats, isLoading: dataStatsLoading } = useSWR(
+    "api/v1/admin/stats",
+    getRequest
+  );
+  const { data: dataHistory, isLoading: dataHistoryLoading } = useSWR(
+    "api/v1/admin/checkout/history?size=8&page=1",
+    getRequest
+  );
+  const { data: dataCourses, isLoading: dataCoursesLoading } = useSWR(
+    "api/v1/admin/courses?size=20&page=1",
+    getRequest
+  );
+
+  const orderData = dataHistory?.data?.map((value) => {
+    return {
+      ...value.course,
+      paid: value?.cancel ? value?.cancel : value?.paid,
+      date: transformDate(value?.created_at),
+    };
+  });
+
   return (
     <section className="flex flex-col gap-6 me-8">
-      <HeaderDashboard />
+      {dataStatsLoading ? (
+        <p>Loading stats...</p>
+      ) : (
+        <HeaderDashboard dataStats={dataStats} />
+      )}
 
       <section className="flex gap-10 ">
-        <OrdersTable newData={dummyData} />
-        <FavoriteCourse />
+        {dataHistoryLoading ? (
+          <p>Loading recent orders...</p>
+        ) : (
+          orderData && <OrdersTable newData={orderData} />
+        )}
+        {dataCoursesLoading ? (
+          <p>Loading favorite courses...</p>
+        ) : (
+          <FavoriteCourse dataCourses={dataCourses} />
+        )}
       </section>
     </section>
   );
