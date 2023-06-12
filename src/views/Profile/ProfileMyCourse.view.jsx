ea636795e8f1cafe1c923567a936da52d4179c01
Rehan Maulana <rehanmaul111@gmail.com>
2023-06-12 11:23:08 +0700

    refactor: API Integration ProfileMyCourse

--- src/views/Profile/ProfileMyCourse.view.jsx
+++ src/views/Profile/ProfileMyCourse.view.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ListCourse from "../../components/organism/ListCourse/ListCourse.organism";
+import useSWR from "swr";
+import { getRequest } from "../../utils/fetcherMethod";
 
 function ProfileMyCourse() {
+  const { data, isLoading } = useSWR("/api/v1/admin/courses?size=10&page=1", getRequest);
+
   return (
     <div>
       <h1 className="text-lg py-4">My Course</h1>
       <div className="h-[32rem] mx-2 overflow-y-auto">
-        <ListCourse />
+       {isLoading ? <p>Loading Course...</p> : <ListCourse allCourse={data} /> } 
       </div>
     </div>
   );
