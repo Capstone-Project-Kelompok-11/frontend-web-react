92f71a0f2dc3a97d87d2ec069693417270d9ea10
Vernando <mayordeblo12@gmail.com>
2023-06-13 17:27:55 +0700

    refactor: add state from useLocation

--- src/views/Course/NewCourse.view.jsx
+++ src/views/Course/NewCourse.view.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import NewCourseForm from "../../components/organism/NewCourseForm/NewCourse.organism";
+import { useLocation } from "react-router-dom";
 
 function NewCourse() {
-  return <NewCourseForm />;
+  const { state } = useLocation();
+  return <NewCourseForm {...state} />;
 }
 
 export default NewCourse;
