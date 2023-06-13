7be88ee3df8980ceb340c37c6a05d2991b134702
Vernando <mayordeblo12@gmail.com>
2023-06-13 17:21:30 +0700

    refactor: add state to newCourse

--- src/components/organism/AllCourse/AllCourse.organism.jsx
+++ src/components/organism/AllCourse/AllCourse.organism.jsx
@@ -10,6 +10,7 @@ function AllCourse({ data }) {
         <p className="text-2xl">All Course</p>
         <Link
           to="/course/new-course"
+          state={{ createNewCourse: true }}
           className="bg-primary-50 hover:bg-primary-70 duration-500 inline-flex text-white  py-2 px-6 rounded-lg text-sm"
         >
           New Course
@@ -18,7 +19,7 @@ function AllCourse({ data }) {
       {listAllCourseCard.length ? (
         <div className="grid grid-cols-2 gap-7 h-[28rem] mt-8 pr-12 bottom-0 pb-10 mr-8 justify-center overflow-y-auto ">
           {data?.map((list) => (
-            <AllCourseCard key={list.id} {...list} />
+            <AllCourseCard key={list.id} data={list} />
           ))}
         </div>
       ) : (
