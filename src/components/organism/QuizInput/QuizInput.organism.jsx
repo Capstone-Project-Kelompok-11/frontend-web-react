b972c652aa2f7bb2f8fe45034c4a778761e73081
achmadhafizd <ahmadhafizd2104@gmail.com>
2023-06-17 21:06:48 +0700

    refactor: delete image from quiz form

--- src/components/organism/QuizInput/QuizInput.organism.jsx
+++ src/components/organism/QuizInput/QuizInput.organism.jsx
@@ -1,137 +1,98 @@
 import React from "react";
-import { useDispatch } from "react-redux";
 import { Field } from "formik";
-import IconImages from "../../atoms/Icons/IconImages.atom";
-import { useQuizImage } from "../../../config/redux/Quiz/quizSelector";
-import quizSlice from "../../../config/redux/Quiz/quizSlice";
 import Trashcan from "../../atoms/Icons/Trashcan.atom";
 import PlusIcon from "../../atoms/Icons/PlusIcon.atom";
 
-function QuizInput({ arrayHelpers, values, setFieldValue }) {
-  const dispatch = useDispatch();
-  const imageSoal = useQuizImage();
-  const onImageChange = (e) => {
-    if (e.target.files && e.target.files[0]) {
-      dispatch(
-        quizSlice.actions.setGambarSoal(URL.createObjectURL(e.target.files[0]))
-      );
-      setFieldValue(e.target.files[0]);
-    }
-  };
-
+function QuizInput({ arrayHelpers, values }) {
   return (
     <div className="w-full max-h-fit overflow-y-auto">
-      {values.question &&
-        values.question.length > 0 &&
-        values.question.map((_, index) => (
+      {values.quizzes &&
+        values.quizzes.length > 0 &&
+        values.quizzes.map((_, index) => (
           <div
             key={index}
             className={`flex py-5 ${
-              (values.question.length > 1 &&
-                index !== values.question.length - 1) ||
-              index !== values.question.length - 1
+              (values.quizzes.length > 1 &&
+                index !== values.quizzes.length - 1) ||
+              index !== values.quizzes.length - 1
                 ? `border-b-2 border-secondary-10`
                 : null
             }`}
           >
-            <div className="flex flex-col">
-              <img src={imageSoal} alt={imageSoal} className="px-6 w-3/6" />
+            <div className="flex flex-col w-5/6">
               <div className="flex flex-col ms-5 me-96">
                 <div className="flex py-5">
                   <Field
-                    name={`question.${index}.Soal${index}`}
-                    className="border-b-2 border-gray-600 h-12 bg-gray-100 w-96 outline-none px-2"
+                    name={`quizzes[${index}].question`}
+                    className="border-b-2 border-gray-600 h-12 bg-gray-100 w-11/12 outline-none px-2"
                     placeholder="Ketik Soal disini"
                   />
-                  <div className="flex items-center justify-center px-10">
-                    <label
-                      htmlFor="dropzone-file"
-                      className="flex flex-col items-center justify-center"
-                    >
-                      <IconImages
-                        width={27}
-                        height={27}
-                        className="hover:bg-gray-100"
-                      />
-                      <Field
-                        id="dropzone-file"
-                        type="file"
-                        className="hidden"
-                        name={`question.${index}.soalImage`}
-                        onChange={onImageChange}
-                      />
-                    </label>
-                  </div>
                 </div>
-                {console.log("index check", index, values.question[index])}
                 <div className="flex flex-col items-start gap-3 py-4 px-5">
                   <div className="flex justify-items-center gap-4">
                     <Field
-                      name={`question.${index}.jawaban`}
+                      name={`quizzes[${index}].valid`}
                       type="radio"
-                      value={values.question[index].input1}
+                      value={values.quizzes[index].text1}
                       className="w-5"
                       checked={
-                        values.question[index].input1 ===
-                        values.question[index].jawaban
+                        values.quizzes[index].text1 ===
+                        values.quizzes[index].valid
                       }
                     />
                     <Field
-                      name={`question.${index}.input1`}
+                      name={`quizzes[${index}].text1`}
                       className="px-2 py-1 outline-none"
                       placeholder="Ketik jawaban disini"
                     />
                   </div>
-
                   <div className="flex justify-items-center gap-4">
                     <Field
-                      name={`question.${index}.jawaban`}
+                      name={`quizzes[${index}].valid`}
                       type="radio"
-                      value={values.question[index].input2}
+                      value={values.quizzes[index].text2}
                       className="w-5"
                       checked={
-                        values.question[index].input2 ===
-                        values.question[index].jawaban
+                        values.quizzes[index].text2 ===
+                        values.quizzes[index].valid
                       }
                     />
                     <Field
-                      name={`question.${index}.input2`}
+                      name={`quizzes[${index}].text2`}
                       className="px-2 py-1 outline-none"
                       placeholder="Ketik jawaban disini"
                     />
                   </div>
-
                   <div className="flex justify-items-center gap-4">
                     <Field
-                      name={`question.${index}.jawaban`}
+                      name={`quizzes[${index}].valid`}
                       type="radio"
-                      value={values.question[index].input3}
+                      value={values.quizzes[index].text3}
                       className="w-5"
                       checked={
-                        values.question[index].input3 ===
-                        values.question[index].jawaban
+                        values.quizzes[index].text3 ===
+                        values.quizzes[index].valid
                       }
                     />
                     <Field
-                      name={`question.${index}.input3`}
+                      name={`quizzes[${index}].text3`}
                       className="px-2 py-1 outline-none"
                       placeholder="Ketik jawaban disini"
                     />
                   </div>
-
                   <div className="flex justify-items-center gap-4">
                     <Field
-                      name={`question.${index}.jawaban`}
+                      name={`quizzes[${index}].valid`}
                       type="radio"
-                      value={values.question[index].input4}
+                      value={values.quizzes[index].text4}
                       className="w-5"
                       checked={
-                        values.question[index].input4 ===
-                        values.question[index].jawaban
+                        values.quizzes[index].text4 ===
+                        values.quizzes[index].valid
                       }
                     />
                     <Field
-                      name={`question.${index}.input4`}
+                      name={`quizzes[${index}].text4`}
                       className="px-2 py-1 outline-none"
                       placeholder="Ketik jawaban disini"
                     />
@@ -142,7 +103,7 @@ function QuizInput({ arrayHelpers, values, setFieldValue }) {
             <div className="flex flex-col border-l-2 border-secondary-10 gap-4 px-4 py-8 w-1/6">
               <button
                 type="button"
-                onClick={() => arrayHelpers.remove(index)} // remove a question from the list
+                onClick={() => arrayHelpers.remove(index)} // remove a quizzes from the list
               >
                 <Trashcan width={24} height={24} />
               </button>
@@ -150,13 +111,14 @@ function QuizInput({ arrayHelpers, values, setFieldValue }) {
                 type="button"
                 onClick={() =>
                   arrayHelpers.push({
-                    input1: "",
-                    input2: "",
-                    input3: "",
-                    input4: "",
-                    jawaban: null,
+                    question: "",
+                    text1: "",
+                    text2: "",
+                    text3: "",
+                    text4: "",
+                    valid: "",
                   })
-                } // insert an empty string at a position
+                }
               >
                 <PlusIcon width={24} height={26} />
               </button>
