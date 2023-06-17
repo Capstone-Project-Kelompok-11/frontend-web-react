27b137b5a67da3ef78381ec512d3f62b0f45eba5
achmadhafizd <ahmadhafizd2104@gmail.com>
2023-06-17 21:05:41 +0700

    refactor: change question to quizzes name

--- src/components/organism/QuizForm/QuizForm.organism.jsx
+++ src/components/organism/QuizForm/QuizForm.organism.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import QuizInput from "../QuizInput/QuizInput.organism";
 import QuizHeader from "../../molecules/QuizHeader/QuizHeader.molecule";
 
-function QuizForm({ values, arrayHelpers, setFieldValue }) {
-  if (values.question.length < 1) {
+function QuizForm({ values, arrayHelpers }) {
+  if (values.quizzes.length < 1) {
     return undefined;
   }
 
@@ -18,16 +18,11 @@ function QuizForm({ values, arrayHelpers, setFieldValue }) {
           SUBMIT
         </button>
       </div>
-      <section className="shadow-sm shadow-gray-500 p-3 min-h-fit me-10 my-6">
+      <section className="shadow-sm shadow-gray-500 p-3 min-h-fit me-10 my-6 ">
         <div className="flex gap-5 justify-between max-h-[480px]">
-          <QuizInput
-            arrayHelpers={arrayHelpers}
-            values={values}
-            setFieldValue={setFieldValue}
-          />
+          <QuizInput arrayHelpers={arrayHelpers} values={values} />
         </div>
       </section>
-      
     </div>
   );
 }
