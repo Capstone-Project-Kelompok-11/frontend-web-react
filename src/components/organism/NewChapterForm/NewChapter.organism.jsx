65b82f10409473d9a9ae26c3f32ca95dbc64febb
Adi1162 <nursholihinadip@gmail.com>
2023-06-19 13:29:48 +0700

    refactor: add integretion api and upload video firebase

--- src/components/organism/NewChapterForm/NewChapter.organism.jsx
+++ src/components/organism/NewChapterForm/NewChapter.organism.jsx
@@ -16,31 +16,12 @@ const NewChapterForm = ({ createNewChapter, id, data = {} }) => {
     () => (createNewChapter ? initCreateNewChapterValue : data),
     []
   );
-  const [url, setUrl] = useState("");
   const [file, setFile] = useState("");
-
   const formik = useFormik({
     initialValues: initData,
     validationSchema: validationCreateNewChapter,
-    onSubmit: async (values) => {
-      if (values) {
-        try {
-          const fetchData = createNewChapter
-            ? postRequest(`/api/v1/admin/module?id=${id}`, {
-                ...values,
-                video: url,
-              })
-            : updateRequest(`/api/v1/admin/module?id=${data.id}`, {
-                ...values,
-                video: url,
-              });
-          await fetchData;
-        } catch (error) {
-          console.log(error.message);
-        }
-        alert("Succes");
-        formik.resetForm();
-      }
+    onSubmit: (values) => {
+      handleUpload(values);
     },
   });
 
@@ -48,33 +29,44 @@ const NewChapterForm = ({ createNewChapter, id, data = {} }) => {
     const file = event.target.files[0];
     setFile(file);
   };
-  const handleUpload = () => {
-    if (file == null) return;
 
+  const handleUpload = (values) => {
+    if (file == null) return;
     const fileRef = storage
       .ref("/videos/" + file.name)
       .put(file)
       .then((snapshot) => {
         getDownloadURL(snapshot.ref)
-          .then((downloadURL) => {
-            setUrl(downloadURL);
+          .then(async (downloadURL) => {
+            try {
+              const link_url = await downloadURL;
+              const fetchData = createNewChapter
+                ? postRequest(`/api/v1/admin/module?id=${id}`, {
+                    ...values,
+                    video: link_url,
+                  })
+                : updateRequest(`/api/v1/admin/module?id=${data.id}`, {
+                    ...values,
+                    video: link_url,
+                  });
+              await fetchData;
+            } catch (error) {
+              console.log(error);
+            }
           })
           .catch((error) => {
             console.error("Error getting download URL:", error);
           });
       });
   };
-
   return (
     <form onSubmit={formik.handleSubmit}>
       <div className="mb-8 flex items-center">
         <p className="text-2xl">New Chapter</p>
-
         <div className="ml-2 transform-origin-center cursor-pointer hover:animate-spin">
           <ArrowPathIcon />
         </div>
       </div>
-
       <div className="w-full flex flex-row gap-6">
         <div className="w-full mr-10">
           <div className="mb-2">
@@ -95,28 +87,28 @@ const NewChapterForm = ({ createNewChapter, id, data = {} }) => {
               onChange={formik.handleChange}
             />
           </div>
-          <div className="mb-4">
-            <label htmlFor="chapterDes">
-              <div className="flex items-center">
-                <label className="font-semibold">Chapter Description</label>
-                <span className="text-red-500 font-semibold">*</span>
-              </div>
-            </label>
-            <textarea
-              id="description"
-              name="description"
-              className="resize-y w-full h-40 mt-3 bg-gray-200 p-5 rounded-lg border border-gray-500"
-              placeholder="Type here..."
-              rows="5"
-              value={formik.values.description}
-              onChange={formik.handleChange}
-            ></textarea>
-          </div>
         </div>
       </div>
       <div className="w-full flex flex-row gap-6">
+        <div className="mb-4">
+          <label htmlFor="chapterDes">
+            <div className="flex items-center">
+              <label className="font-semibold">Chapter Description</label>
+              <span className="text-red-500 font-semibold">*</span>
+            </div>
+          </label>
+          <textarea
+            id="description"
+            name="description"
+            className="resize-y w-[701px] h-[390px] mt-3 bg-gray-200 p-5 rounded-lg border border-gray-500"
+            placeholder="Type here..."
+            rows="5"
+            value={formik.values.description}
+            onChange={formik.handleChange}
+          ></textarea>
+        </div>
         <div className="w-80 mr-8">
-          <div className="mb-2  flex items-center">
+          <div className="mb-2 flex items-center">
             <p className="font-semibold">Uploaded Videos</p>
           </div>
           <div className="w-full h-40 bg-gray-200 flex items-center justify-center rounded-lg">
@@ -124,10 +116,10 @@ const NewChapterForm = ({ createNewChapter, id, data = {} }) => {
               <div className="flex items-center justify-center w-full">
                 <label
                   htmlFor="dropzone-file"
-                  className="flex flex-col items-center justify-center w-full h-64 "
+                  className="flex flex-col items-center justify-center w-full h-64"
                 >
                   <div className="flex flex-col items-center justify-center pt-5 pb-5">
-                    <span className="text-center  text-gray-500">Link</span>
+                    <span className="text-center text-gray-500">Link</span>
                   </div>
                   <input
                     id="video"
@@ -143,12 +135,11 @@ const NewChapterForm = ({ createNewChapter, id, data = {} }) => {
           </div>
         </div>
       </div>
-
       <div className="flex font-semibold mt-6">
         <button
           type="button"
           onClick={() => navigate(-1)}
-          className="justify-start  bg-warning-10 hover:bg-warning-30 text-black py-2 px-6 rounded-lg text-sm fw-bold"
+          className="justify-start bg-warning-10 hover:bg-warning-30 text-black py-2 px-6 rounded-lg text-sm fw-bold"
         >
           Back
         </button>
