e53b91bd3182038e43fb4884e2394967f93e334e
Rehan Maulana <rehanmaul111@gmail.com>
2023-06-21 16:53:37 +0700

    chore: delete whitespace

--- src/components/organism/ProfileSideBar/ProfileSideBar.organism.jsx
+++ src/components/organism/ProfileSideBar/ProfileSideBar.organism.jsx
@@ -15,11 +15,11 @@ function ProfileSideBar() {
   const date = transformDate(data?.data?.bod || "2023-06-10")
   
   return (
-    <div className="fixed block inset-x-0 top-0 py-24 bg-primary-30 w-72 h-screen ">
+    <div className="fixed block inset-x-0 top-0 py-24 bg-primary-30 w-72 h-screen">
       <div className="text-center px-10 flex-row">
         <ProfileAccount {...data?.data} />
         <div className="flex flex-col items-start my-4">
-          <h1 className="font-bold text-sm py-1 ">Contact Information</h1>
+          <h1 className="font-bold text-sm py-1">Contact Information</h1>
           <EmailDash text={data?.data?.email} />
           <PhoneDash phone={data?.data?.phone} />
           <DateOfBirthDash birthDate={date} />
