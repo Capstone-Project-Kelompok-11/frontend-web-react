5ba52927b285b9cec289e68103b932f096d32d17
Rehan Maulana <rehanmaul111@gmail.com>
2023-06-21 16:18:49 +0700

    chore: delete unused whitespace

--- src/components/organism/AllCourse/AllCourse.organism.jsx
+++ src/components/organism/AllCourse/AllCourse.organism.jsx
@@ -10,13 +10,13 @@ function AllCourse({ data }) {
         <Link
           to="/course/new-course"
           state={{ createNewCourse: true }}
-          className="bg-primary-50 hover:bg-primary-70 duration-500 inline-flex text-white  py-2 px-6 rounded-lg text-sm"
+          className="bg-primary-50 hover:bg-primary-70 duration-500 inline-flex text-white py-2 px-6 rounded-lg text-sm"
         >
           New Course
         </Link>
       </div>
       {data?.length ? (
-        <div className="grid grid-cols-2 gap-7 h-[28rem] mt-8 pr-12 bottom-0 pb-10 mr-8 justify-center overflow-y-auto ">
+        <div className="grid grid-cols-2 gap-7 h-[28rem] mt-8 pr-12 bottom-0 pb-10 mr-8 justify-center overflow-y-auto">
           {data?.map((list) => (
             <AllCourseCard key={list.id} data={list} />
           ))}
