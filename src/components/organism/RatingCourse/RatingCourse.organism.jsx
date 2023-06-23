34e7842779c4fcd74c729b8f007e3138a823641f
Rehan Maulana <rehanmaul111@gmail.com>
2023-06-23 19:48:59 +0700

    style: change style on Rating Course

--- src/components/organism/RatingCourse/RatingCourse.organism.jsx
+++ src/components/organism/RatingCourse/RatingCourse.organism.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import { listRating } from "./dummyData";
 import RatingCourseCard from "../RatingCourseCard/RatingCourseCard.organism";
 
-function RatingCourse() {
+function RatingCourse({comment}) {
+  
   return (
     <div className="bg-light-blue/20 rounded-xl drop-shadow-lg shadow-lg -mr-2">
       <div className="p-4">
         <h1 className="font-bold pb-6">Rating Course</h1>
-        {listRating.map((list) => (
+
+        <div className="h-[36rem]">
+        {comment?.map((list) => (
           <RatingCourseCard key={list.id} list={list} />
         ))}
+        </div>
+       
       </div>
     </div>
   );
