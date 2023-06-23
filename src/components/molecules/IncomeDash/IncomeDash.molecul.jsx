cd99d458172c98409a9a5e308227e1d84c8e1518
Rehan Maulana <rehanmaul111@gmail.com>
2023-06-23 22:02:15 +0700

    chore: delete text "Juta"

--- src/components/molecules/IncomeDash/IncomeDash.molecul.jsx
+++ src/components/molecules/IncomeDash/IncomeDash.molecul.jsx
@@ -6,11 +6,11 @@ function IncomeDash({ newIncome }) {
     <div className="inline-flex flex-col p-2 mx-3 font-bold rounded-xl text-white w-56 bg-[url('../src/assets/bgProfile.jpg')]">
       <div className="flex px-2 my-2 justify-between">
         <h1 className="pb-4 text-2xl">Income</h1>
-        <div className=" rounded-full h-9 pt-2 my-2 p-2 bg-white bg-opacity-30">
+        <div className="rounded-full h-9 pt-2 my-2 p-2 bg-white bg-opacity-30">
           <IncomeIcon />
         </div>
       </div>
-      <p className="text-3xl px-4 py-2">Rp {newIncome} Juta</p>
+      <p className="text-3xl px-4 py-2">Rp {newIncome}</p>
     </div>
   );
 }
