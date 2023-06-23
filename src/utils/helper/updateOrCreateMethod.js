56506bbe2ba10f39aa9ef99f1a4af1b484e66ac6
Rehan Maulana <rehanmaul111@gmail.com>
2023-06-23 19:55:46 +0700

    feat: add new update or create method

---
+++ src/utils/helper/updateOrCreateMethod.js
@@ -0,0 +1,52 @@
+export const handleUpdateOrCreateChapter = async ({
+  createNewChapter,
+  values,
+  id,
+  data,
+  link_url,
+  postRequest,
+  updateRequest,
+}) => {
+  if (createNewChapter) {
+    await postRequest(`/api/v1/admin/module?id=${id}`, {
+      ...values,
+      video: link_url,
+    });
+  }
+
+  await updateRequest(`/api/v1/admin/module?id=${data.id}`, {
+    ...values,
+    video: link_url,
+  });
+};
+
+const handleUploadImage = async (res, file, uploadImage) => {
+  await uploadImage(
+    `/api/v1/admin/course/thumbnail/upload?id=${res.data.id}`,
+    file
+  );
+  await uploadImage(
+    `/api/v1/admin/course/thumbnail/upload?id=${res.data.id}`,
+    file
+  );
+};
+
+export const handleUpdateOrCreateCourse = async ({
+  createNewCourse,
+  values,
+  postRequest,
+  updateRequest,
+  id,
+  uploadImage,
+  file,
+}) => {
+  if (createNewCourse) {
+    const res = await postRequest("/api/v1/admin/course", values);
+    await handleUploadImage(res, file, uploadImage);
+  }
+  const resUpdate = await updateRequest(
+    `/api/v1/admin/course?id=${id}`,
+    values
+  );
+  await handleUploadImage(resUpdate, file, uploadImage);
+};
